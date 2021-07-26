2d61983d964e93a55c56a28c8a74ee377172e493
Dirk Speelman <d.speelman@keesingtechnologies.com>
2021-07-26 18:48:36 +0200

    Add apikey check

--- owm/index.js
+++ owm/index.js
@@ -1,12 +1,40 @@
 // npm install node-fetch
 const fetch = require("node-fetch");
 const owm = require('./owm.js');
+const { promises: { readFile } } = require("fs");
 
 const API_SERVICE_URL = "https://api.openweathermap.org/data/2.5/onecall";
+
+let apikeys = {};
+let isValidApikey = async function (apikey) {
+    if (!apikeys.keys) {
+        let json = {};
+        await readFile('apikeys/apikeys.json').then(fileBuffer => {
+            // console.log(fileBuffer.toString());
+            json = fileBuffer.toString();
+        }).catch(error => {
+            console.error(error.message);
+        });
+        apikeys = JSON.parse(json);
+    }
+    return apikeys.keys.indexOf(apikey) > -1;
+}
 
 module.exports = async function (context, req) {
     try {
         context.log('HTTP trigger function processed a request: ' + req.originalUrl);
+
+        // authorization
+        if (!req.headers.authorization) {
+            context.res = { status: 401, body: "" };
+            return;
+        }
+
+        let allowed = await isValidApikey(req.headers.authorization);
+        if (!allowed) {
+            context.res = { status: 403, body: "" };
+            return;
+        }
 
         // lat, lon, appid must exist
         if (!req.query.lat || !req.query.lon || !req.query.appid) {
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Simple Open Weather Map proxy
+
+Convert the json result of the `OneCall` api to a smaller version to support the garmin connect-iq `What Weather OWM` app.
+
+
+
+## Api keys config to access the proxy
+example file: apikeys.json
+```
+{
+    "keys" : [
+        "xxxxxxxxxxxxxx"
+    ]
+}
+```
